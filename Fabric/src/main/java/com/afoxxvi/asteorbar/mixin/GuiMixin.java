db4f49165483366f3d253341030240a6fdbbe651
afoxxvi <unconfigured@null.spigotmc.org>
2024-02-21 23:45:58 +0800

    fix fabric config, adjust config keys, translations

--- Fabric/src/main/java/com/afoxxvi/asteorbar/mixin/GuiMixin.java
+++ Fabric/src/main/java/com/afoxxvi/asteorbar/mixin/GuiMixin.java
@@ -6,13 +6,21 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphics;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(Gui.class)
 public abstract class GuiMixin {
+    @Invoker("renderPlayerHealth")
+    public abstract void renderPlayerHealthRaw(GuiGraphics guiGraphics);
+
     @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/Gui;renderPlayerHealth(Lnet/minecraft/client/gui/GuiGraphics;)V"), method = "render")
     public void renderPlayerHealth(Gui instance, GuiGraphics guiGraphics) {
+        if (Overlays.style == Overlays.STYLE_NONE) {
+            renderPlayerHealthRaw(guiGraphics);
+            return;
+        }
         Overlays.reset();
         var mc = Minecraft.getInstance();
         var tick = 0f;
@@ -28,14 +36,22 @@ public abstract class GuiMixin {
         Overlays.STRING.render(gui, guiGraphics, tick, width, height);
     }
 
+    @Invoker("renderVehicleHealth")
+    public abstract void renderVehicleHealthRaw(GuiGraphics guiGraphics);
+
     @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/Gui;renderVehicleHealth(Lnet/minecraft/client/gui/GuiGraphics;)V"), method = "render")
     public void renderVehicleHealth(Gui instance, GuiGraphics guiGraphics) {
-
+        if (Overlays.style == Overlays.STYLE_NONE) {
+            renderVehicleHealthRaw(guiGraphics);
+        }
     }
+
 
     @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/Gui;renderExperienceBar(Lnet/minecraft/client/gui/GuiGraphics;I)V"), method = "render")
     public void renderExperienceBar(Gui instance, GuiGraphics guiGraphics, int i) {
-        //instance.renderExperienceBar(poseStack, i);
+        if (Overlays.style == Overlays.STYLE_NONE) {
+            instance.renderExperienceBar(guiGraphics, i);
+        }
     }
 }
 

df94ee2d92683b49d1f355d46c6c5f4295a200da
afoxxvi <afoxxvi@outlook.com>
2025-03-12 18:08:36 +0800

    [feature] mealapi compat

--- Fabric/src/main/java/com/afoxxvi/asteorbar/config/FabricConfigAdapter.java
+++ Fabric/src/main/java/com/afoxxvi/asteorbar/config/FabricConfigAdapter.java
@@ -543,6 +543,11 @@ public class FabricConfigAdapter implements ConfigAdapter {
         return config.hook.hookThermoo;
     }
 
+    @Override
+    public boolean hookMealApi() {
+        return config.hook.hookMealApi;
+    }
+
     public static void init() {
         AutoConfig.register(FabricConfigAdapter.AsteorBarConfig.class, JanksonConfigSerializer::new);
         holder = AutoConfig.getConfigHolder(FabricConfigAdapter.AsteorBarConfig.class);
@@ -777,6 +782,8 @@ public class FabricConfigAdapter implements ConfigAdapter {
             public boolean hookApoli = DefaultConfigAdapter.I.hookApoli();
             @Comment(ConfigComment.hookThermoo)
             public boolean hookThermoo = DefaultConfigAdapter.I.hookThermoo();
+            @Comment(ConfigComment.hookMealAPI)
+            public boolean hookMealApi = DefaultConfigAdapter.I.hookMealApi();
         }
     }
 }
